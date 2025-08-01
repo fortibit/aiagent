33fda73f68d3391bdeb1e6cbdf420129305e6f47
fortibit <robdomu@gmail.com>
2025-08-01 21:54:35 +0200

    Chore

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from dotenv import load_dotenv
 from google import genai
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
 def main():
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
     print(response.text)
 
     prompt_tokens = response.usage_metadata.prompt_token_count
