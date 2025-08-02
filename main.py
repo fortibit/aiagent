121d8d844bcd63340481b873885a22a19e741275
fortibit <robdomu@gmail.com>
2025-08-02 20:17:13 +0200

    Add verbose option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -11,21 +12,31 @@ def main():
     client = genai.Client(api_key=api_key)
     # get user prompt from argument
     try:
-        prompt = sys.argv[1]
+        user_prompt = sys.argv[1]
     except:
         print("Usage: python3 main.py prompt")
         sys.exit(1)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
-    print("Response: ")
-    print(response.text)
 
+    # get tokens count
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+
+    # check --verbose option
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
+    print("Response: ")
+    print(response.text)
 
 
 if __name__ == "__main__":
