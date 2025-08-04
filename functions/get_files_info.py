08e64d50f65f0ac94e93dab13abfe1c3b569a81f
fortibit <robdomu@gmail.com>
2025-08-04 10:31:53 +0200

    Add tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,10 +6,9 @@ def get_files_info(working_directory, directory="."): # example "calculator", "p
 
     # check if the absolute path is outside of working_directory
     if working_directory not in absolute_path:
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return f'    Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     
     print(full_path)
     print(absolute_path)
-    return f"Result for current directory:\n - main.py: file_size=575 bytes, is_dir=False\n - tests.py: file_size=1342 bytes, is_dir=False\n - pkg: file_size=4096 bytes, is_dir=True"
+    return None
--- tests.py
+++ tests.py
@@ -10,11 +10,40 @@ calculator_pkg_size = os.path.getsize("./calculator/pkg")
 calculator_calculator_py_size = os.path.getsize("./calculator/pkg/calculator.py")
 calculator_render_py_size = os.path.getsize("./calculator/pkg/render.py")
 
+
 class TestGetFilesInfo(unittest.TestCase):
 
     def test_current_directory(self):
-        result = f"Result for current directory:\n - main.py: file_size={calculator_main_py_size} bytes, is_dir=False\n - tests.py: file_size={calculator_tests_py_size} bytes, is_dir=False\n - pkg: file_size={calculator_pkg_size} bytes, is_dir=True"
+        result = (
+            f"Result for current directory:\n"
+            + " - main.py: file_size={calculator_main_py_size} bytes, is_dir=False\n"
+            + " - tests.py: file_size={calculator_tests_py_size} bytes, is_dir=False\n"
+            + " - pkg: file_size={calculator_pkg_size} bytes, is_dir=True"
+        )
         func_result = get_files_info("calculator", ".")
+        self.assertEqual(func_result, result)
+
+    def test_pkg_directory(self):
+        result = (
+            f"Result for 'pkg' directory:\n"
+            + " - calculator.py: file_size={calculator_calculator_py_size} bytes, is_dir=False\n"
+            + " - render.py: file_size={calculator_render_py_size} bytes, is_dir=False"
+        )
+        func_result = get_files_info("calculator", "pkg")
+        self.assertEqual(func_result, result)
+
+    def test_outside_working_directory(self):
+        result = (
+            f"Result for '/bin' directory:\n"
+            + '    Error: Cannot list "/bin" as it is outside the permitted working directory\n'
+        )
+        func_result = get_files_info("calculator", "/bin")
+        self.assertEqual(func_result, result)
+        result = (
+            f"Result for '../' directory:\n"
+            + '    Error: Cannot list "/bin" as it is outside the permitted working directory\n'
+        )
+        func_result = get_files_info("calculator", "../")
         self.assertEqual(func_result, result)
 
 
